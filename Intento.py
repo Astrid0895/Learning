36ef972ac47720011282de5226fb3198f274ffff
Astrid Palacios <asyopahe@gmail.com>
2023-07-31 07:18:19 -0500

    Cambio actividad de suma

---
+++ Intento.py
@@ -0,0 +1,4 @@
+n = input()
+n = int(n)
+suma = (2*n*(n+1))//2
+print(suma)
--- Suma.py
+++ Suma.py
@@ -10,11 +10,9 @@ sum = primer_numero + segundo_numero + tercer_numero + cuarto_numero
 print(f"La suma de los primeros n numeros pares es: {sum}")
 
 
-#Segundo intento
-def suma_primeros_n_pares(n):
-    suma = n* (n+1)
-    return suma
+#Segundo intento, despues de entender la operacion
 
-n = int(input('Ingrese el valor de n (numero entero): '))
-resultado = suma_primeros_n_pares(n)
-print(f'La suma de los primeros {n} numeros pares es: {resultado}')
+n = input()
+n = int(n)
+suma = (2*n*(n+1))//2
+print(suma)
